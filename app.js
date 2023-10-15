c8a483cd3ae05949005844a9c0568a8ca553ece8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-10-15 18:53:48 +0530

    get balance

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ let { Web3 } = require("web3");
 const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
 let web3 = new Web3(provider);
 
-console.log(web3);
+console.log(
+  web3.eth
+    .getBalance("0xc2f67316af7225287196ba2f88a73f20ee42213b")
+    .then((bal) => console.log(bal))
+);
+
+// console.log(
+//   web3.eth
+//     .getBalance("0xc2f67316af7225287196ba2f88a73f20ee42213b")
+//     .then((bal) => console.log(web3.utils.fromWei(bal, "ether")))
+// );
